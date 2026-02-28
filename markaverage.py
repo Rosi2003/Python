67e98a0ad0d3f1643caa409e5ee23a140f80a771
Namadee Umedya <namadeeumedya2002@gmail.com>
2026-02-28 11:50:49 +0530

    Added mark average logic

---
+++ markaverage.py
@@ -0,0 +1,6 @@
+M1 = float(input("Enter first mark: "))
+M2 = float(input("Enter second mark: "))
+M3 = float(input("Enter third mark: "))
+
+average = (M1 + M2 + M3) / 3
+print(f"The average of the three marks is: {average}")
